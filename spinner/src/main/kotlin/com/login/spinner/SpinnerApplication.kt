6eb3236a1fbe48125bfeb715670ba4feabb82a2d
Derick <dboehm2019@outlook.com>
2022-12-11 11:58:57 -0600

    Initial load.... Again

---
+++ spinner/src/main/kotlin/com/login/spinner/SpinnerApplication.kt
@@ -0,0 +1,11 @@
+package com.login.spinner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpinnerApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpinnerApplication>(*args)
+}
---
+++ spinner/src/test/kotlin/com/login/spinner/SpinnerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.login.spinner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpinnerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
